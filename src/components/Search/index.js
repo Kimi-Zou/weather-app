51918edaca1174cc4ae75cbd5f18c937b3e46631
Kimi Zou <kimizou.kz@gmail.com>
2021-04-26 00:35:18 -0400

    Add GooglePlacesAutocomplete Component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,8 +1,41 @@
+import { Component, useEffect, useState } from 'react';
+import GooglePlacesAutocomplete, {geocodeByPlaceId} from 'react-google-places-autocomplete';
+import './index.css';
+
 const Search = () => {
+  const [value, setValue] = useState(null);
+  // const [lat, setLat] = useState(null);
+  // const [lng, setLng] = useState(null);
+
+  useEffect(() => {
+    if (value) {
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${value.value.place_id}&key=${process.env.REACT_APP_GOOGLE_API}`)
+      .then(res => res.json())
+      .then(data => fetch(`https://api.weather.gov/points/${data.results[0].geometry.location.lat.toFixed(4)},${data.results[0].geometry.location.lng.toFixed(4)}`))
+      .then(res => res.json())
+      .then(data => console.log(data))
+    }
+  }, [value])
+
   return (
     <div>
-      <i className="fas fa-search-location"></i>
-      <input type='search' />
+      <GooglePlacesAutocomplete
+        className='search'
+        apiKey='***REMOVED***'
+        autocompletionRequest={{
+          country: ['us']
+        }}
+        selectProps={{
+          value,
+          onChange: setValue,
+          placeholder: 'Enter your location...',
+          className: 'search--input',
+          components: {
+            DropdownIndicator:() => null, 
+            IndicatorSeparator:() => null 
+          }
+        }}
+      />
     </div>
   )
 } 
